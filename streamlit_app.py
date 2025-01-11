1a4703ba420a5d3274a3f95e930d1e4b0ceef7da
neo-highlander <slamet.koesno@gmail.com>
2025-01-11 23:23:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("🎈 Hello World")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
